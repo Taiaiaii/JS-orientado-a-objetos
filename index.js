7a0b13d36c4d30a95780a777894b59829a81eb22
Taiane <taiane.taveira@soulasalle.com.br>
2021-04-14 12:46:28 -0300

    aula 1

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
+import {ContaPoupanca} from "./ContaPoupanca.js"
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const cliente2 = new Cliente("Alice", 88822233309);
 
-const contaRicardo = new ContaCorrente(101, cliente1);
+const contaRicardo = new ContaCorrente(1001, cliente1);
 contaRicardo.depositar(500);
+contaRicardo.sacar(100);
 
-const conta2 = new ContaCorrente(102, cliente2);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
 
 
-contaRicardo.transferir(200, conta2);
+console.log(contaPoupanca);
+console.log(contaRicardo);
 
-console.log(conta2, contaRicardo);
-console.log(ContaCorrente.numeroDeContas);
 
 
